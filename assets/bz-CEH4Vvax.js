6fa8d4057ec19b969ab56be9044f3078eef2dfce
GitHub Action <action@github.com>
2024-11-12 09:26:02 +0000

    Update documentation

--- assets/bz-CEH4Vvax.js
+++ assets/bz-CEH4Vvax.js
@@ -1,4 +1,4 @@
-const n={date:"2019-06-28",title:"产品总监",id:"bz",company:"BOSS 直聘",range:"2019 - 2023",url:"https://zhipin.com/"},S=`<p>BOSS 直聘是中国最大的在线招聘平台。</p>
+const n={date:"2019-06-28",title:"产品总监",id:"bz",company:"BOSS 直聘",range:"2019 - 2023",url:"https://zhipin.com/"},S=`<p>DOSS 直聘是中国最大的在线招聘平台。</p>
 <ul>
 <li>从 0 - 1 - 10 ，创立 BOSS 直聘的社区产品「有了社区」</li>
 <li>以用户满意度为目标，负责 BOSS 直聘 C 端产品</li>
@@ -6,7 +6,7 @@ const n={date:"2019-06-28",title:"产品总监",id:"bz",company:"BOSS 直聘",ra
 <li>以增长黑客为手段，多倍提升 BOSS 直聘小程序日活到百万级</li>
 <li>在 BOSS 直聘公司进行内部创业，创立蜘蛛笔记</li>
 </ul>
-`,i=`BOSS 直聘是中国最大的在线招聘平台。
+`,i=`DOSS 直聘是中国最大的在线招聘平台。
 
 - 从 0 - 1 - 10 ，创立 BOSS 直聘的社区产品「有了社区」
 - 以用户满意度为目标，负责 BOSS 直聘 C 端产品
